d21267fc747e02d2c3f7bd465f16d19d9785871e
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-05 11:43:25 +0530

    If statements

---
+++ src/decision_structures/SalaryCalculator.java
@@ -0,0 +1,27 @@
+package decision_structures;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args) {
+        //Initialize known values
+        int salary =  1000;
+        int bonus = 250;
+        int quota = 10;
+
+        //Get values for unknown
+        System.out.println("How many sales did employee make this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        //Bonus earners
+        if(sales > quota){
+            salary = salary + bonus;
+        }
+
+        //Output
+        System.out.println("The employee pay is $" + salary);
+    }
+}
