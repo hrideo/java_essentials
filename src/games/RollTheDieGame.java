126342359917237bcd61715068954d09e7bdc52c
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-06 14:51:01 +0530

    Challenge Roll Die Revision1

--- src/games/RollTheDieGame.java
+++ src/games/RollTheDieGame.java
@@ -5,31 +5,32 @@ import java.util.Random;
 public class RollTheDieGame {
 
     public static void main(String[] args){
-
         int maxRolls = 5;
         int currentSpace = 0;
         int lastSpace = 20;
         Random random = new Random();
 
         System.out.println("Welcome to Roll the die game!");
-        for(int i=0; i<=maxRolls; i++){
+
+        for(int i=0; i<maxRolls; i++){
 
             int die = random.nextInt(6) + 1;
-            currentSpace = currentSpace +die;
+            currentSpace = currentSpace + die;
 
-            lastSpace = lastSpace - die;
-            System.out.println("Roll #" + (i+1) + ":" + " You've rolled a " + die + ".");
+            System.out.print(String.format("Roll #%d. You've rolled a %d. ", (i+1), die));
 
-            if(currentSpace<20){
-                System.out.println("You are now on space " + currentSpace + " and have "
-                        + lastSpace + " more to go.");
-            }
-            else if(currentSpace==20){
+            if(currentSpace==lastSpace){
                 System.out.println("You are on space " + currentSpace + ". Congrats you win.");
                 break;
             }
+            else if(currentSpace>lastSpace){
+                System.out.println("Unfortunately, that takes you past " + lastSpace + " spaces. You lose.");
+                break;
+            }
             else{
-                System.out.println("Invalid space: " + currentSpace);
+                int spacesToGo = lastSpace - currentSpace;
+                System.out.println("You are now on space " + currentSpace + " and have "
+                        + spacesToGo + " more to go.");
             }
         }
 
