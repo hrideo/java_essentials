e586272e18b92103e5f9ba5b626e671dba6e278b
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-10 09:36:25 +0530

    Encapsulation- Defining classes for objects

--- src/objects/Rectangle.java
+++ src/objects/Rectangle.java
@@ -1,27 +1,29 @@
 package objects;
 
 public class Rectangle {
-    double length;
-    double width;
+    //encapsulation- private access modifiers to fields(data), public access modifiers to methods(functions)
 
-    double calculatePerimeter(){
+    private double length;
+    private double width;
+
+    public double calculatePerimeter(){
         return (2 * length) + (2 *  width);
     }
-    double calculateArea(){
+    public double calculateArea(){
         return length * width;
     }
 
-    double getLength(){
+    public double getLength(){
         return length;
     }
-    void setLength(double length){
+    public void setLength(double length){
         this.length = length;
     }
 
-    double getWidth(){
+    public double getWidth(){
         return width;
     }
-    void setWidth(double width){
+    public void setWidth(double width){
         this.width = width;
     }
 }
