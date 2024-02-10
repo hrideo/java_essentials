9aa74d656c5e41b945be9fd5aed1e0ef1d60931e
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-10 11:13:47 +0530

    Constructors

--- src/objects/Rectangle.java
+++ src/objects/Rectangle.java
@@ -6,6 +6,20 @@ public class Rectangle {
     private double length;
     private double width;
 
+    //in addition to using setter to assign values to fields, 'constructors' can also be used
+
+    // default constructor- blank constructor that does nothing, even if not declared still it exists
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    //our declared constructor for creating Rectangle object with length width
+    public Rectangle(double length, double width){
+        setLength(length);
+        setWidth(width);
+    }
+
     public double calculatePerimeter(){
         return (2 * length) + (2 *  width);
     }
