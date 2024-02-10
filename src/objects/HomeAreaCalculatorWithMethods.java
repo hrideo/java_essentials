69c5d961563ce692d37c932863aaf5d6b3c2c658
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-10 18:57:14 +0530

    Objects as method return types

--- src/objects/HomeAreaCalculatorWithMethods.java
+++ src/objects/HomeAreaCalculatorWithMethods.java
@@ -1,15 +1,29 @@
 package objects;
 
+import java.util.Scanner;
+
 public class HomeAreaCalculatorWithMethods {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main (String[] args){
-        Rectangle kitchen = new Rectangle(300, 700);
-        Rectangle bathroom = new Rectangle(200, 400);
+        Rectangle kitchen = getRoom();
+        Rectangle bathroom = getRoom();
         double totalArea = calculateTotalArea(kitchen, bathroom);
         System.out.println(totalArea);
     }
 
     public static double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2){
         return rectangle1.calculateArea() + rectangle2.calculateArea();
+    }
+
+    public static Rectangle getRoom(){
+        System.out.println("Enter the length of room");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter width of room");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length, width);
     }
 }
