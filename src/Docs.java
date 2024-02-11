58f6e51b0e2cb108a472d8c5610e86b9752c45fd
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-11 11:29:12 +0530

    Wrapper classes

---
+++ src/Docs.java
@@ -0,0 +1,7 @@
+public class Docs {
+    /*Wrapper classes: object representation of primitive data type
+
+     -Benefit of using wrapper as opposed to primitive data types is the ability to use convenience methods
+    */
+
+}
