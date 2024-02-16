5977d990b5d4d967677fa0e5b3b8950ab5cc6380
hrideo <148361819+hrideo@users.noreply.github.com>
2024-02-16 13:40:58 +0530

    Strings

---
+++ src/text_processing/TextProcessor.java
@@ -0,0 +1,13 @@
+/*
+Strings: are not primitive data type, and they are also not wrapper class
+- string is an object that is sequence of characters
+- we can access a string just like we would access an array of characters
+ */
+package text_processing;
+
+public class TextProcessor {
+
+    public static void main(String[] args){
+
+    }
+}
