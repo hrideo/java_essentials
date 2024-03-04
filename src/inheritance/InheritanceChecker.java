b75bb066916ba84e51069c9cb6f5506f05dd913f
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-04 11:31:37 +0530

    Inheritance between classes

---
+++ src/inheritance/InheritanceChecker.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class InheritanceChecker {
+
+    public static void main(String[] args){
+        Person person1 = new Person();
+        //person1.
+        Employee employee1 = new Employee();
+        //employee1.
+    }
+}
