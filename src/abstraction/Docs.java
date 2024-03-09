938642b067e6ae95974c47179d404e688eefe489
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-09 13:16:21 +0530

    Abstract classes and methods

---
+++ src/abstraction/Docs.java
@@ -0,0 +1,17 @@
+/*
+Abstraction: is something that exists in theory but does not have concrete existence
+-in Java we have reserved word abstract
+-abstract is non-access modifier
+-It can be used when we want to define a template for a class or method, but do not intend for it to be used as is.
+
+-an Abstract class is not designed to be instantiated, but it's designed to be extended, thus pushing the burden of implementation onto the subclass
+-any class that contains an abstract method must be abstract itself
+-an abstract class can contain implemented methods as well
+
+-an Abstract method has no body, only signature of method is defined, and is not designed to be run as is, it
+is designed to be overwritten by a subclass
+ */
+package abstraction;
+
+public class Docs {
+}
---
+++ src/abstraction/Shapes.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public abstract class Shapes {
+
+    abstract double calculateArea();
+
+    public void print(){
+        System.out.println("I am a shape");
+    }
+}
