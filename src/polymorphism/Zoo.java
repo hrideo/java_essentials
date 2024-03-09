5f980d98b59eb38c8b2137c141d7152008caef04
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-09 12:55:30 +0530

    instanceof operator

--- src/polymorphism/Zoo.java
+++ src/polymorphism/Zoo.java
@@ -11,6 +11,7 @@ public class Zoo {
         Dog rocky = new Dog();
         rocky.fetch();
         rocky.makeSound();
+        feed(rocky);
 
         //when we create an object with a type of superclass but instantiated with type of subclass, this
         // is an implicit up cast
@@ -31,10 +32,28 @@ public class Zoo {
         //this does not change overall object, sasha is still of type Animal, but in this specific call sasha will
         // be type of cat just so we can execute scratch method
         ((Cat)sasha).scratch();
+        feed(sasha);
 
         //this was just temporary, if we wanted to store a down casted object, we could do so by assigning the cast
         // to an object of that same type
-        Cat sashaTheCat = (Cat)sasha;
-        sashaTheCat.scratch();
+        //Cat sashaTheCat = (Cat)sasha;
+        //sashaTheCat.scratch();
     }
+
+    //instanceof is a handy operator that checks if an object is an instance of a specific class
+    public static void feed(Animal animal){
+        if(animal instanceof Dog){
+            System.out.println("here's dog food");
+        }
+        else if(animal instanceof Cat){
+            System.out.println("here's cat food");
+        }
+    }
+    //instanceof operator is useful and comes in handy if we want to safely cast an object
+    //e.g. Animal sasha = new Dog();
+    //if(sasha instanceof Dog sashaTheDog){
+    //    sashaTheDog.fetch();
+    //}
+    //here we check if sasha is dog, but if so, we can cast Sasha as a dog and store it in new variable sashaTheDog,
+    //this is called pattern matching and allows to perform two actions in one
 }
