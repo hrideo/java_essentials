5c0bc52a7400b7d2f0a818eb322a98eee79013ab
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-09 11:09:36 +0530

    Polymorphism

---
+++ src/polymorphism/Docs.java
@@ -0,0 +1,12 @@
+/*
+Polymorphism: is where subclass can define their own unique behaviors and yet share some of the same behaviors of
+ the super class
+
+-Type vs Instance: an object can have a superclass type and a subclass instance
+-Overridden methods: if a method is overridden by the subclass, the polymorphic object will execute the
+overridden method at runtime
+ */
+package polymorphism;
+
+public class Docs {
+}
