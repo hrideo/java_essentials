82bf346ec5bb4490ab6c43409ac571160f22f5f1
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-16 09:40:33 +0530

    Data Structures

---
+++ src/collections/Docs.java
@@ -0,0 +1,13 @@
+/*
+Collection is an object that holds references to other objects.
+Collection are considered data structures and the objects within a collection are known as elements.
+
+Elements within a Collection:
+-Unique vs duplication
+-Ordered vs unordered
+
+ */
+package collections;
+
+public class Docs {
+}
