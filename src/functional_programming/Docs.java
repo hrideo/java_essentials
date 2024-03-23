bf840bbe5b780ab44f6764c0299b1cd7ec58fd24
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-23 19:24:22 +0530

    Functional programming

---
+++ src/functional_programming/Docs.java
@@ -0,0 +1,7 @@
+/*
+Functional programming: is a software development style that uses mathematical functions to transform input into output.
+ */
+package functional_programming;
+
+public class Docs {
+}
