e33526b5b9e2c712e85c568fe54463a3683d24e0
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-25 11:29:56 +0530

    Streams

---
+++ src/functional_programming/Streams.java
@@ -0,0 +1,13 @@
+/*
+In addition to the java.util.function package, Java also provides the java.util.stream package for functional programming needs.
+
+Stream: represents a sequence of elements from a collection, array or other input source.
+-stream does not store elements like collections do, but instead, allows us to perform operations on the data without modifying the original source.
+
+The stream API also provides a method to process operations on the elements in parallel. When doing so, multiple threads are used to process the elements of the stream at the same time.
+The stream API is a powerful tool for data processing.
+ */
+package functional_programming;
+
+public class Streams {
+}
