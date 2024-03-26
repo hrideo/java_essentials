f2b3841244e975e34433098117de5c4188b35245
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-26 18:52:55 +0530

    Exception Handling

---
+++ src/exceptions/Docs.java
@@ -0,0 +1,11 @@
+/*
+Exception is an event that occurs at runtime due to an error.
+An exception disrupts the normal flow of a program.
+
+Exception handling gives us greater control over the user experience of our applications.
+We can handle exceptions within our code so that, instead of allowing the program to crash we handle exception in a graceful manner.
+ */
+package exceptions;
+
+public class Docs {
+}
