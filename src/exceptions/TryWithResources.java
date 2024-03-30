078c9b106b9c8c739209f979b874d0fa5e1b7212
hrideo <148361819+hrideo@users.noreply.github.com>
2024-03-30 09:30:18 +0530

    Try with resources- multiple resources

--- src/exceptions/TryWithResources.java
+++ src/exceptions/TryWithResources.java
@@ -2,6 +2,8 @@
 A better approach to closing the resources in a finally block is to use try-with-resources
 try-with-resources allows us to declare resources that should be automatically closed after execution of try block
 To try-with-resources we use set of parentheses after the word try, and inside declare and initialize resources
+We can also specify multiple resources to close, add a semicolon after first resource
+These multiple resources will be closed in the reversed order that they were defined
  */
 package exceptions;
 
@@ -9,16 +11,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class TryWithResources {
 
     public static void main(String[] args){
         File inputFile = new File("files/numbers.txt");
+        File outputFile = new File("files/output.txt");
 
-        try(Scanner fileReader = new Scanner(inputFile)){
+        try(
+                Scanner fileReader = new Scanner(inputFile);
+                //PrintWriter to write output to file
+                PrintWriter fileWriter = new PrintWriter(outputFile);
+        ){
 
             while(fileReader.hasNext()){
-                System.out.println(fileReader.nextDouble());
+                //changing System.out to fileWriter
+                fileWriter.println(fileReader.nextDouble());
             }
         }catch(FileNotFoundException | InputMismatchException e) {
             e.printStackTrace();
